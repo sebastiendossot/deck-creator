dddd38449be48989657b536e68a6e64138038d4c
Sebastien Dossot <sebastien.dossot@gmail.com>
2020-03-26 09:16:12 -0600

    Simple version playable

--- docker_django/frontend/src/components/player/Player.js
+++ docker_django/frontend/src/components/player/Player.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getCards, deleteCard }  from '../../actions/cards';
 import { withRouter } from "react-router";
+import { Redirect } from 'react-router-dom';
+
 
 export class Player extends Component {
   static propTypes = {
@@ -15,7 +17,8 @@ export class Player extends Component {
   state = {
     questionShown: true,
     currentCard: null,
-    indexCurrentCard: 0
+    indexCurrentCard: 0, 
+    endDeck: false
   }
 
   componentDidMount(){
@@ -26,9 +29,38 @@ export class Player extends Component {
     this.setState({currentCard: nextProps.cards[0]})
   }
 
+  cardRemembered(){
+    //things if card remembered
+    this.nextCard();
+  }
+
+  cardForgotten(){
+    // things if card forgotten
+    this.nextCard();
+  }
+
+  nextCard(){
+    const nextIndex = this.state.indexCurrentCard + 1;
+    const cards = this.props.cards;
+    if(nextIndex<cards.length){
+      this.setState({currentCard: cards[nextIndex], indexCurrentCard: nextIndex})
+    }else{
+      this.setState({endDeck: true})
+      // come back if end of deck.
+    }
+  }
 
   render() {
-    const {questionShown, currentCard} = this.state;
+    const {questionShown, currentCard, endDeck} = this.state;
+    // const cardRemembered = this.props.cardRemembered;
+    // const cardForgotten= this.props.cardForgotten;
+
+    if (endDeck) {
+      return <Redirect to={{
+            pathname: `/`
+        }} push={true} />
+    }
+
     return (
       <Fragment>
         <h2>
@@ -40,9 +72,9 @@ export class Player extends Component {
               <div className="card-body">
                 <h5 className={`card-title ${questionShown ? '' : 'hidden'}`}>{currentCard.question}</h5>
                 <h5 className={`card-title ${questionShown ? 'hidden' : ''}`}>{currentCard.answer}</h5>
-                <button className="btn btn-primary">I got it</button>
-                <button className="btn btn-warning">I can't remember</button>
-                <button className="btn btn-info">Show answer</button>
+                <button className="btn btn-primary"  onClick={() => this.cardRemembered()} >I got it</button>
+                <button className="btn btn-warning" onClick={() => this.cardForgotten()}>I can't remember</button>
+                <button className="btn btn-info" onClick={() => this.setState({questionShown: !this.state.questionShown})}>{`Show ${questionShown ? 'answer' : 'question'}`}</button>
               </div>
             </div>
             :
